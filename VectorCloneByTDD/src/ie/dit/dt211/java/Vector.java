635d69175ae890b3366dbbd9b7e7c819c360a864
Brian Gillespie <brian.x.gillespie@dit.ie>
2012-10-05 22:08:22 +0100

    Starter code for TDD Vector lab exercise

---
+++ VectorCloneByTDD/src/ie/dit/dt211/java/Vector.java
@@ -0,0 +1,9 @@
+package ie.dit.dt211.java;
+
+public class Vector {
+
+	public Vector() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ VectorCloneByTDD/src/ie/dit/dt211/java/VectorTest.java
@@ -0,0 +1,31 @@
+package ie.dit.dt211.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/*
+ * Good idea to list the interfaces to be tested here and tick them
+ * off as they are completed
+ *
+ * 
+ * [ ] new Vector()
+ * [ ] boolean add(Object o)
+ * [ } void add(int index, Object o)
+ * [ ] Object get(int index)
+ * [ ] boolean remove(Object o)
+ * [ ] void clear()
+ * [ ] int size()
+ * [ ] int capacity()
+ * [ ] boolean contains(Object o)
+ * [ ] int indexOf()
+ * [ ] int lastIndexOf()
+ */
+public class VectorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
